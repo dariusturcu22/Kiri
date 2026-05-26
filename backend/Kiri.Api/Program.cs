bbf5ae2acb5211133cba6f7563998881232678f0
dariusturcu22 <protdm22@gmail.com>
2026-05-26 16:50:09 +0300

    chore: enable Swagger in all environments for demo

--- backend/Kiri.Api/Program.cs
+++ backend/Kiri.Api/Program.cs
@@ -139,11 +139,8 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors();
 app.UseAuthentication();
